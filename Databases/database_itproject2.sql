5767c486f435cf688c370e92c282e20fbc97ff79
AldanaDestine <37394486+AldanaDestine@users.noreply.github.com>
2019-10-02 14:41:04 +0800

    Add files via upload

--- Databases/database_itproject2.sql
+++ Databases/database_itproject2.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 27, 2019 at 06:58 PM
--- Server version: 10.4.6-MariaDB
--- PHP Version: 7.3.9
+-- Generation Time: Oct 02, 2019 at 08:39 AM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 7.3.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -21,6 +21,53 @@ SET time_zone = "+00:00";
 --
 -- Database: `database_itproject2`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `accounts`
+--
+
+CREATE TABLE `accounts` (
+  `userID` varchar(15) NOT NULL,
+  `userType` varchar(50) NOT NULL,
+  `firstName` varchar(50) NOT NULL,
+  `surName` varchar(50) NOT NULL,
+  `office` varchar(50) NOT NULL,
+  `org` varchar(50) NOT NULL,
+  `position` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `accounts`
+--
+
+INSERT INTO `accounts` (`userID`, `userType`, `firstName`, `surName`, `office`, `org`, `position`, `email`, `password`) VALUES
+('2164482', 'Student', 'Jayditch', 'Balansi', 'SAMCIS', 'SICAP', 'President', '2164482@slu.edu.ph', '1234jayditch');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `facilities`
+--
+
+CREATE TABLE `facilities` (
+  `facilityID` varchar(15) NOT NULL,
+  `facilityLevel` varchar(15) NOT NULL,
+  `facilityRoom` varchar(15) NOT NULL,
+  `roomType` varchar(50) NOT NULL,
+  `roomDescription` varchar(50) NOT NULL,
+  `roomCapacity` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `facilities`
+--
+
+INSERT INTO `facilities` (`facilityID`, `facilityLevel`, `facilityRoom`, `roomType`, `roomDescription`, `roomCapacity`) VALUES
+('4438', 'mezzanine', 'mezzanine', 'others', 'event', '2000');
 
 -- --------------------------------------------------------
 
@@ -45,6 +92,18 @@ INSERT INTO `super_user` (`idSU`, `uidSU`, `emailSU`, `pwdSU`) VALUES
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `accounts`
+--
+ALTER TABLE `accounts`
+  ADD PRIMARY KEY (`userID`);
+
+--
+-- Indexes for table `facilities`
+--
+ALTER TABLE `facilities`
+  ADD PRIMARY KEY (`facilityID`);
 
 --
 -- Indexes for table `super_user`
