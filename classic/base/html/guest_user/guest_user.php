929462e5251c9a551d2b33514e0190ea1777ef55
patricksabado <2168760@slu.edu.ph>
2019-10-16 11:51:33 +0800

    added a temporary logout for guest user

--- classic/base/html/guest_user/guest_user.php
+++ classic/base/html/guest_user/guest_user.php
@@ -17,6 +17,8 @@
     <link rel="stylesheet" href="../../../global/css/bootstrap.min.css">
     <link rel="stylesheet" href="../../../global/css/bootstrap-extend.min.css">
     <link rel="stylesheet" href="../../assets1/css/site.min.css">
+    <link rel="stylesheet" href="../guest_user/css/custom.css">
+    
     
     <!-- Plugins -->
     <link rel="stylesheet" href="../../../global/vendor/animsition/animsition.css">
@@ -79,11 +81,6 @@
             title="Remark">
           <span class="navbar-brand-text hidden-xs-down"> Guest User</span>
         </a>
-        <button type="button" class="navbar-toggler collapsed" data-target="#site-navbar-search"
-          data-toggle="collapse">
-          <span class="sr-only">Toggle Search</span>
-          <i class="icon wb-search" aria-hidden="true"></i>
-        </button>
       </div>
     
       <div class="navbar-container container-fluid">
@@ -107,104 +104,17 @@
             <li class="nav-item dropdown">
               <a class="nav-link navbar-avatar" data-toggle="dropdown" href="#" aria-expanded="false"
                 data-animation="scale-up" role="button">
-                <span class="avatar avatar-online">
-                  <img src="../../../global/portraits/5.jpg" alt="...">
-                  <i></i>
+                <span>
+                  <img src="../guest_user/images/icon/dropdown.svg" width="10px" height="10px">
                 </span>
               </a>
               <div class="dropdown-menu" role="menu">
-                <a class="dropdown-item" href="javascript:void(0)" role="menuitem"><i class="icon wb-power" aria-hidden="true"></i> Logout</a>
-              </div>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link" data-toggle="dropdown" href="javascript:void(0)" title="Notifications"
-                aria-expanded="false" data-animation="scale-up" role="button">
-                <i class="icon wb-bell" aria-hidden="true"></i>
-                <span class="badge badge-pill badge-danger up">5</span>
-              </a>
-              <div class="dropdown-menu dropdown-menu-right dropdown-menu-media" role="menu">
-                <div class="dropdown-menu-header">
-                  <h5>NOTIFICATIONS</h5>
-                  <span class="badge badge-round badge-danger">New 5</span>
-                </div>
-    
-                <div class="list-group">
-                  <div data-role="container">
-                    <div data-role="content">
-                      <a class="list-group-item dropdown-item" href="javascript:void(0)" role="menuitem">
-                        <div class="media">
-                          <div class="pr-10">
-                            <i class="icon wb-order bg-red-600 white icon-circle" aria-hidden="true"></i>
-                          </div>
-                          <div class="media-body">
-                            <h6 class="media-heading">A new order has been placed</h6>
-                            <time class="media-meta" datetime="2018-06-12T20:50:48+08:00">5 hours ago</time>
-                          </div>
-                        </div>
-                      </a>
-                      <a class="list-group-item dropdown-item" href="javascript:void(0)" role="menuitem">
-                        <div class="media">
-                          <div class="pr-10">
-                            <i class="icon wb-user bg-green-600 white icon-circle" aria-hidden="true"></i>
-                          </div>
-                          <div class="media-body">
-                            <h6 class="media-heading">Completed the task</h6>
-                            <time class="media-meta" datetime="2018-06-11T18:29:20+08:00">2 days ago</time>
-                          </div>
-                        </div>
-                      </a>
-                      <a class="list-group-item dropdown-item" href="javascript:void(0)" role="menuitem">
-                        <div class="media">
-                          <div class="pr-10">
-                            <i class="icon wb-settings bg-red-600 white icon-circle" aria-hidden="true"></i>
-                          </div>
-                          <div class="media-body">
-                            <h6 class="media-heading">Settings updated</h6>
-                            <time class="media-meta" datetime="2018-06-11T14:05:00+08:00">2 days ago</time>
-                          </div>
-                        </div>
-                      </a>
-                      <a class="list-group-item dropdown-item" href="javascript:void(0)" role="menuitem">
-                        <div class="media">
-                          <div class="pr-10">
-                            <i class="icon wb-calendar bg-blue-600 white icon-circle" aria-hidden="true"></i>
-                          </div>
-                          <div class="media-body">
-                            <h6 class="media-heading">Event started</h6>
-                            <time class="media-meta" datetime="2018-06-10T13:50:18+08:00">3 days ago</time>
-                          </div>
-                        </div>
-                      </a>
-                      <a class="list-group-item dropdown-item" href="javascript:void(0)" role="menuitem">
-                        <div class="media">
-                          <div class="pr-10">
-                            <i class="icon wb-chat bg-orange-600 white icon-circle" aria-hidden="true"></i>
-                          </div>
-                          <div class="media-body">
-                            <h6 class="media-heading">Message received</h6>
-                            <time class="media-meta" datetime="2018-06-10T12:34:48+08:00">3 days ago</time>
-                          </div>
-                        </div>
-                      </a>
-                    </div>
-                  </div>
-                </div>
-                <div class="dropdown-menu-footer">
-                  <a class="dropdown-menu-footer-btn" href="javascript:void(0)" role="button">
-                    <i class="icon wb-settings" aria-hidden="true"></i>
-                  </a>
-                  <a class="dropdown-item" href="javascript:void(0)" role="menuitem">
-                    All notifications
-                  </a>
-                </div>
+                <a class="dropdown-item" href="../guest_user/auth/logout.php" role="menuitem"><i class="icon wb-power" aria-hidden="true"></i> Logout</a>
               </div>
             </li>
           </ul>
           <!-- End Navbar Toolbar Right -->
         </div>
-        <!-- End Navbar Collapse -->
-    
-        <!-- Site Navbar Seach -->
         <div class="collapse navbar-search-overlap" id="site-navbar-search">
           <form role="search">
             <div class="form-group">
