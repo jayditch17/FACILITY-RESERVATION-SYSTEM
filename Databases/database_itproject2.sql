45ae258327cf0c3db8c4ec29cfaac6458535f8fc
jayditch17 <37394517+jayditch17@users.noreply.github.com>
2019-11-13 02:21:15 +0800

    database (auto inc. primary keys)

--- Databases/database_itproject2.sql
+++ Databases/database_itproject2.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 4.9.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 14, 2019 at 09:16 AM
--- Server version: 10.1.37-MariaDB
--- PHP Version: 7.2.12
+-- Generation Time: Nov 12, 2019 at 07:19 PM
+-- Server version: 10.4.6-MariaDB
+-- PHP Version: 7.3.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -29,13 +29,20 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `account_fac` (
-  `userID` int(10) NOT NULL,
-  `first_name` varchar(50) NOT NULL,
-  `last_name` varchar(50) NOT NULL,
-  `deptFac` varchar(50) NOT NULL,
-  `facEmail` varchar(50) NOT NULL,
-  `facPassword` varchar(50) NOT NULL
+  `userID` int(11) NOT NULL,
+  `first_name` varchar(20) NOT NULL,
+  `last_name` varchar(20) NOT NULL,
+  `deptFac` varchar(20) NOT NULL,
+  `facEmail` varchar(20) NOT NULL,
+  `facPass` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `account_fac`
+--
+
+INSERT INTO `account_fac` (`userID`, `first_name`, `last_name`, `deptFac`, `facEmail`, `facPass`) VALUES
+(3, 'jayditch', 'balansi', 'sao', 'awdawdawkll', '12345');
 
 -- --------------------------------------------------------
 
@@ -44,13 +51,20 @@ CREATE TABLE `account_fac` (
 --
 
 CREATE TABLE `account_office` (
-  `officeID` int(15) NOT NULL,
+  `officeID` int(20) NOT NULL,
   `firstName` varchar(20) NOT NULL,
   `lastName` varchar(20) NOT NULL,
   `depOfc` varchar(20) NOT NULL,
-  `ofcEmail` varchar(50) NOT NULL,
-  `ofcPass` varchar(25) NOT NULL
+  `email` varchar(50) NOT NULL,
+  `password` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `account_office`
+--
+
+INSERT INTO `account_office` (`officeID`, `firstName`, `lastName`, `depOfc`, `email`, `password`) VALUES
+(3, 'sample', 'sample', 'sao', 'office@slu.edu.ph', 'officefs');
 
 -- --------------------------------------------------------
 
@@ -59,28 +73,21 @@ CREATE TABLE `account_office` (
 --
 
 CREATE TABLE `account_orgs` (
-  `userID` varchar(15) NOT NULL,
+  `userID` int(15) NOT NULL,
   `firstName` varchar(50) NOT NULL,
   `lastName` varchar(50) NOT NULL,
   `studOrg` varchar(50) NOT NULL,
-  `studPostition` varchar(50) NOT NULL,
+  `studPosition` varchar(50) NOT NULL,
   `studEmail` varchar(50) NOT NULL,
   `studPassword` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
 --
--- Table structure for table `account_thirdpart`
+-- Dumping data for table `account_orgs`
 --
 
-CREATE TABLE `account_thirdpart` (
-  `userID` int(10) NOT NULL,
-  `firstName` varchar(100) NOT NULL,
-  `lastName` varchar(100) NOT NULL,
-  `userEmail` varchar(100) NOT NULL,
-  `userPass` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+INSERT INTO `account_orgs` (`userID`, `firstName`, `lastName`, `studOrg`, `studPosition`, `studEmail`, `studPassword`) VALUES
+(12, 'localhost', 'localhost', 'localhost', 'localhost', 'student@slu.edu.ph', 'studentfs');
 
 -- --------------------------------------------------------
 
@@ -95,7 +102,7 @@ CREATE TABLE `events` (
   `eventOrg` varchar(15) NOT NULL,
   `actEve` varchar(50) NOT NULL,
   `actVenue` varchar(50) NOT NULL,
-  `numPart` int(10) NOT NULL,
+  `numPart` varchar(10) NOT NULL,
   `startDate` date NOT NULL,
   `endDate` date NOT NULL,
   `startTime` time(6) NOT NULL,
@@ -107,9 +114,7 @@ CREATE TABLE `events` (
 --
 
 INSERT INTO `events` (`eventID`, `firstName`, `lastName`, `eventOrg`, `actEve`, `actVenue`, `numPart`, `startDate`, `endDate`, `startTime`, `endTime`) VALUES
-(110, 'jay', 'ditch', 'rpg', 'sportsfest', 'courts', 90, '2019-11-01', '2019-11-14', '14:00:00.000000', '23:00:00.000000'),
-(989, 'Jayditch', 'Balansi', 'sicap', 'fund raising', 'oval', 90, '0000-00-00', '0000-00-00', '07:00:00.000000', '16:00:00.000000'),
-(3387, 'Jayditch', 'Hakdog', 'jpia', 'sportsfest', 'oval ', 89, '2019-10-23', '2019-10-29', '09:00:00.000000', '15:00:00.000000');
+(5, 'h', 'gjhg', 'jhg', 'jhg', 'jhg', 'jhg', '0000-00-00', '0000-00-00', '00:00:00.000000', '00:00:00.000000');
 
 -- --------------------------------------------------------
 
@@ -131,28 +136,14 @@ CREATE TABLE `facilities` (
 --
 
 INSERT INTO `facilities` (`facilityID`, `facilityLevel`, `facilityRoom`, `roomType`, `roomDescription`, `roomCapacity`) VALUES
-(2, '1', '555', 'Lecture', 'Thesis natin', '10'),
-(3, '5', '515', 'Lecture', 'meeting meetingmeeting meetingmeeting meetingmeeti', '50');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `reservation`
---
-
-CREATE TABLE `reservation` (
-  `eventID` int(10) NOT NULL,
-  `eventDate` date NOT NULL,
-  `eventName` varchar(100) NOT NULL,
-  `dateStart` date NOT NULL,
-  `dateEnd` date NOT NULL,
-  `timeStart` time(6) NOT NULL,
-  `timeEnd` time(6) NOT NULL,
-  `Organization` varchar(100) NOT NULL,
-  `Position` varchar(100) NOT NULL,
-  `Department` varchar(100) NOT NULL,
-  `noParticipant` int(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+(6, '4', '433', 'Clinic', 'dwadefdsf', '50'),
+(7, '4', '433', 'Clinic', 'dwadefdsf', '50'),
+(8, '4', '433', 'Court', 'dwadefdsf', '50'),
+(9, '4', '433', 'Laboratory', 'wda', '50'),
+(10, '100', '100', 'Laboratory', 'dwadefdsf', '500'),
+(11, '5', '515', 'Lecture', 'ayus', '500'),
+(12, '5', '433', 'Laboratory', 'lecturelecturelecturelecturelecture', '50'),
+(13, '100', '100', 'Lecture', 'wdwadawdawdawd', '4141');
 
 -- --------------------------------------------------------
 
@@ -179,6 +170,12 @@ INSERT INTO `super_user` (`idSU`, `uidSU`, `emailSU`, `pwdSU`) VALUES
 --
 
 --
+-- Indexes for table `account_fac`
+--
+ALTER TABLE `account_fac`
+  ADD PRIMARY KEY (`userID`);
+
+--
 -- Indexes for table `account_office`
 --
 ALTER TABLE `account_office`
@@ -191,22 +188,16 @@ ALTER TABLE `account_orgs`
   ADD PRIMARY KEY (`userID`);
 
 --
--- Indexes for table `account_thirdpart`
---
-ALTER TABLE `account_thirdpart`
-  ADD PRIMARY KEY (`userID`);
-
---
 -- Indexes for table `events`
 --
 ALTER TABLE `events`
   ADD PRIMARY KEY (`eventID`);
 
 --
--- Indexes for table `reservation`
+-- Indexes for table `facilities`
 --
-ALTER TABLE `reservation`
-  ADD PRIMARY KEY (`eventID`);
+ALTER TABLE `facilities`
+  ADD PRIMARY KEY (`facilityID`);
 
 --
 -- Indexes for table `super_user`
@@ -219,22 +210,34 @@ ALTER TABLE `super_user`
 --
 
 --
+-- AUTO_INCREMENT for table `account_fac`
+--
+ALTER TABLE `account_fac`
+  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT for table `account_office`
 --
 ALTER TABLE `account_office`
-  MODIFY `officeID` int(15) NOT NULL AUTO_INCREMENT;
+  MODIFY `officeID` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT for table `account_thirdpart`
+-- AUTO_INCREMENT for table `account_orgs`
 --
-ALTER TABLE `account_thirdpart`
-  MODIFY `userID` int(10) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `account_orgs`
+  MODIFY `userID` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
--- AUTO_INCREMENT for table `reservation`
+-- AUTO_INCREMENT for table `events`
 --
-ALTER TABLE `reservation`
-  MODIFY `eventID` int(10) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `events`
+  MODIFY `eventID` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `facilities`
+--
+ALTER TABLE `facilities`
+  MODIFY `facilityID` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `super_user`
