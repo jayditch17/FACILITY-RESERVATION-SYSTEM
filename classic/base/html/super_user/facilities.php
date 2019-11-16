7f0d63cee5292f6e87596ada6415d20c63d3b3ce
lesterdeguzman <32890425+lesterdeguzman@users.noreply.github.com>
2019-11-16 18:20:36 +0800

    Update facilities.php

--- classic/base/html/super_user/facilities.php
+++ classic/base/html/super_user/facilities.php
@@ -351,7 +351,7 @@
                 <div class="modal-content">
                   <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                    <h4 class="modal-title"><span class="glyphicon glyphicon-plus-sign"></span> Add Member</h4>
+                    <h4 class="modal-title"><span class="glyphicon glyphicon-plus-sign"></span> Add Facility</h4>
                   </div>
                   
                   <form class="form-horizontal" action="php_action/create.php" method="POST" id="createMemberForm">
